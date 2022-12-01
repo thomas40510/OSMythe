70dedbf91923600f7e4f90e68f6b90a6fc0dc00b
Thomas Prévost <thomasprevost85@gmail.com>
2022-12-01 12:12:15 +0100

    tp4/ init tp4

---
+++ tp4/thread1.c
@@ -0,0 +1,34 @@
+// ------------------------------------------------------------------
+// exemple-pthread-create-1.c
+// Fichier d'exemple du livre "Developpement Systeme sous Linux"
+// (C) 2000-2019 - Christophe BLAESS <christophe@blaess.fr>
+// https://www.blaess.fr/christophe/
+// ------------------------------------------------------------------
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void *thread_function(void *arg)
+{
+	while (1) {
+		fprintf(stderr, "New thread\n");
+		sleep(1);
+	}
+}
+
+
+int main (void)
+{
+	pthread_t thr;
+
+	if (pthread_create(&thr, NULL, thread_function, NULL) != 0) {
+		fprintf(stderr, "Error during pthread_create()\n");
+		exit(EXIT_FAILURE);
+	}
+	while (1) {
+		fprintf(stderr, "Main thread\n");
+		sleep(1);
+	}
+}
