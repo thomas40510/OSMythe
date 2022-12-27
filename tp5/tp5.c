9a4072420207a8029292ee4bb6587bb31f321202
Thomas Prévost <thomasprevost85@gmail.com>
2022-12-27 13:04:00 +0100

    tp5/ product

--- tp5/tp5.c
+++ tp5/tp5.c
@@ -11,44 +11,29 @@
 /* --- Exo 1 ---
  * Endless loop that prints the number of the CPU it is running on.
  */
-//void exo1(){
-//    int i = 0;
-//    while(1){
-//        printf("CPU %d\n", sched_getcpu());
-//        sleep(2);
-//        i++;
-//    }
-//}
-//
-///* --- Exo 2 ---
-// * Execute a function on a specific CPU.
-// */
-//void exo2(void){
-//    cpu_set_t cpuset;
-//    CPU_ZERO(&cpuset);
-//    CPU_SET(1, &cpuset);
-//    sched_setaffinity(0, sizeof(cpu_set_t), &cpuset);
-//    exo1();
-//}
-
-/* --- Exo 3 ---
- * Produit scalaire en équilibrant la charge sur les CPU.
- */
-void exo3(){
-    //long nb_procs = sysconf(_SC_NPROCESSORS_ONLN);
-
-    int vector_size = 4;
-
-    double *v1 = malloc(vector_size * sizeof(double));
-    double *v2 = malloc(vector_size * sizeof(double));
-    double *v3 = malloc(vector_size * sizeof(double));
-
-
-    printf("%s", (const char *) v1);
+void exo1(){
+    int i = 0;
+    while(1){
+        printf("CPU %d\n", sched_getcpu());
+        sleep(2);
+        i++;
+    }
 }
 
+/* --- Exo 2 ---
+ * Execute a function on a specific CPU.
+ */
+void exo2(void){
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(1, &cpuset);
+    sched_setaffinity(0, sizeof(cpu_set_t), &cpuset);
+    exo1();
+}
+
+
 int main(int argc, char *argv[]){
-    exo3();
+    exo2();
     printf("OK");
     return 0;
 }
